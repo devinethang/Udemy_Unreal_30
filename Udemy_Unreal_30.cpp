7bf58863581edc56ede0f0c8d0b4b22ae82a8a3e
Devin Graupmann <devinethang@gmail.com>
2019-09-02 16:20:53 -0700

    Generate Random Numbers.

--- Udemy_Unreal_30.cpp
+++ Udemy_Unreal_30.cpp
@@ -2,10 +2,33 @@
 //
 
 #include "stdafx.h"
+#include <iostream> 
+#include <time.h>
 
+using namespace std;
+
+int GenerateRandomNumber(int range)
+{
+	return rand() % (range+1);
+}
 
 int main()
-{
+{ 
+	srand(time(0));
+
+	// loop this many times 
+	const int count = 20;
+
+	cout << "Generate " << count << " random numbers." << endl;
+	
+	for (int i = 0; i < count; ++i)
+	{
+		// This wil generate a number from 0 to 100
+		int number = GenerateRandomNumber(100);
+
+		cout << "Generated: " << number << endl;
+	}
+
     return 0;
 }
 
